386a4bd8f550794c0ef57078f638bf8f0597b969
Szysz Lukasz <lukasz.szysz@pentacomp.pl>
2026-05-11 14:51:44 +0200

    Init

---
+++ src/main/java/pl/flaaaxxx/eskatebackaned/ESkateBackanedApplication.java
@@ -0,0 +1,13 @@
+package pl.flaaaxxx.eskatebackaned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESkateBackanedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESkateBackanedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/flaaaxxx/eskatebackaned/ESkateBackanedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.flaaaxxx.eskatebackaned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESkateBackanedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
