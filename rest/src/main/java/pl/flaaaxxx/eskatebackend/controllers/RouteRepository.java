af751b7e8d3bfdb324e137fbe882e4de0765baff
Szysz Lukasz <lukasz.szysz@pentacomp.pl>
2026-05-12 07:54:55 +0200

    Zapis jooqowy tabel

--- rest/src/main/java/pl/flaaaxxx/eskatebackend/controllers/RouteRepository.java
+++ rest/src/main/java/pl/flaaaxxx/eskatebackend/controllers/RouteRepository.java
@@ -1,45 +1,49 @@
 package pl.flaaaxxx.eskatebackend.controllers;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
+import pl.flaaaxxx.eskatebackend.tables.Routes;
+
+import java.math.BigDecimal;
+
 import static org.jooq.impl.DSL.*;
+import static pl.flaaaxxx.eskatebackend.Tables.ROUTES;
 
 @Repository
+@RequiredArgsConstructor
 public class RouteRepository {
 
     private final DSLContext dsl;
 
-    public RouteRepository(DSLContext dsl) {
-        this.dsl = dsl;
-    }
-
     public Record getFullRoute(String id) {
         return dsl.select(
-                        field("id"),
-                        field("name"),
-                        field("start_date"),
-                        field("total_distance"),
-                        field("unit"),
-                        field("ST_AsGeoJSON(path)").as("geometry") // Mapujemy geometrię na alias
+                        ROUTES.ID,
+                        ROUTES.NAME,
+                        ROUTES.START_DATE,
+                        ROUTES.TOTAL_DISTANCE,
+                        ROUTES.UNIT,
+                        field("ST_AsGeoJSON(" + ROUTES.PATH + ")").as("geometry") // Mapujemy geometrię na alias
                 )
-                .from(table("routes"))
-                .where(field("id").eq(id))
+                .from(ROUTES)
+                .where(ROUTES.ID.eq(id))
                 .fetchOne();
     }
 
     @Transactional
     public void save(RouteDto dto, String wktPath) {
         dsl.insertInto(table("routes"))
-                .set(field("id"), dto.getId())
-                .set(field("name"), dto.getProperties().getName())
-                .set(field("start_date"), dto.getProperties().getDate())
-                .set(field("total_distance"), dto.getProperties().getDistance())
-                .set(field("unit"), dto.getProperties().getUnit())
+                .set(ROUTES.ID, dto.getId())
+                .set(ROUTES.NAME, dto.getProperties().getName())
+                .set(ROUTES.START_DATE, dto.getProperties().getDate())
+                .set(ROUTES.TOTAL_DISTANCE, dto.getProperties().getDistance() != null
+                        ? BigDecimal.valueOf(dto.getProperties().getDistance())
+                        : null)                .set(ROUTES.UNIT, dto.getProperties().getUnit())
                 // Konwersja Stringa WKT na typ GEOMETRY w MySQL
-                .set(field("path"), DSL.field("ST_GeomFromText({0}, 4326)", String.class, wktPath))
+                .set(ROUTES.PATH, DSL.field("ST_GeomFromText({0}, 4326)", ROUTES.PATH.getType(), wktPath))
                 .execute();
     }
 }
